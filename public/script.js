a90ae4c38675e6ed686dd0a8f886ed66de48a077
Jereen <jader9e1@hotmail.com>
2023-01-06 17:55:43 -0600

    "Payment featues added and reviews, verification and public page styled"

--- public/script.js
+++ public/script.js
@@ -1,2 +1,27 @@
-// const button = document.querySelector('button')
-// button.addEventListener("click")
+const button = document.querySelector('button')
+button.addEventListener("click", ()=>{
+    fetch('/check-out', {
+        method:'Post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            itemed:
+                {id:1, quantity: 3},
+                
+            
+        }),
+    })
+        .then(res =>{
+            //if there is a failure then sends it to a promise reject
+            if (res.ok) return res.json()
+            return res.json().then(json => Promise.reject(json))
+        }).then(({url})=>{
+            // console.log(url);
+            window.location = url
+        }).catch(e => {
+            //sends an error from server if there is an error
+            console.error(e.error)
+        })
+    })
+
