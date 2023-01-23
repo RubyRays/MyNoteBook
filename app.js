685c2efeb12563d4a3add4374ae8f3d74dd20014
Jereen <jader9e1@hotmail.com>
2023-01-23 08:11:10 -0600

    "feat: Move pages routes and public-pages routes to their own js files"

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const passport = require("passport");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // const findOrCreate = require('mongoose-findorcreate');
 const _ = require("lodash");
-const data = require(__dirname+"/data.js");
 const https = require("https");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
@@ -24,23 +23,29 @@ const {google, dlp_v2} = require ("googleapis");
 const { OAuth2Client } = require('google-auth-library');
 const OAuth2 = google.auth.OAuth2
 const stripe= require('stripe')(process.env.STRIPE_PRIVATE_KEY);
-//modules and middleware---------------
+//modules---------------
 const Note = require('./models/Note');
 const Review = require("./models/Review");
 const NoteUser= require('./models/NoteUser');
-const {isLoggedIn} = require('./middleware/login_middlewaare');
-const {level1Access, level2Access}= require('./middleware/access_middleware');
 const Subscription = require('./models/Subscription');
 const Session = require('./models/Session');
+//middleware-----------------------------
+const {isLoggedIn} = require('./middleware/login_middlewaare');
+const {level1Access, level2Access}= require('./middleware/access_middleware');
 const { query } = require('express');
 const ErrorHandle = ('./ErrorHandle.js');
 //-----------------
 //--helper functions for ejs
 const instaClick = require('./clickBtn.js');
+//OAuth routes
 const oAuth2Client= new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
 oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
-
 const app = express();
+
+//Routes
+const pages = require('./routes/pages.js');
+const public= require('./routes/public-pages');
+
 
 //---------cloudinary configuration-------------------------
 cloudinary.config({
@@ -182,6 +187,8 @@ app.use(function(err, req, res, next) {
     next(err)
 })
 
+app.use("/pages", pages);
+app.use("/public-pages", public);
 
 
 //--HOME ROUTE----
@@ -432,472 +439,12 @@ app.post("/verify-email", isLoggedIn, async (req, res)=> {
 
 
 
-//-------------------MAIN PAGE FOR USER NOTES--------------------
-app.get("/pages", isLoggedIn, async(req,res)=>{
 
 
-        const theUser = req.user.username;
 
 
-        //finds the current user using the user.id provided by the passport package
-        NoteUser.findById(req.user.id,function(err, foundUser) {
-            if(err){
-                console.log(err);
-            }else{
 
-                if(foundUser){
 
-                    //looking for the data in the notes collection where the owner name
-                    //is the same as the username of the currently logged in user
-                    Note.find({"owner": req.user.username, "deleted":{$ne:"true"}}, function(err, user2) {
-
-                        const pic= foundUser.profileImage.url;
-                                            
-                        //saves the notes associated with the user into the notebooks array
-                        foundUser.noteBookContents=user2;
-
-                        //saves the userinfo into noteUser collection and renders the page
-                        foundUser.save(function(){
-
-                            res.render("page", {pic, messages: req.flash('success'), userContent: foundUser, theUser: theUser});
-                                
-                        })
-                    })
-                }
-            }
-        });
-  
-
-});
-
-//renders the notebook user page
-app.post("/pages",isLoggedIn, async(req,res)=>{
-    
-//API SECTION WHERE DATA IS PASSED FROM ONE API TO BE USED IN THE OTHER BY MEANS OF 
-//CALLBACK FUNCTIONS  
-    //USING AN IP ADDRESS FINDER TO GET THE CITY NAME OF THE USER
-    token=process.env.IPINFO_TOKEN;
-    const ipinfoApi ="https://ipinfo.io?token="+token;
-
-    function makeCall (ipinfoApi, callback){
-
-        https.get(ipinfoApi, function(response){
-
-            // console.log(response.statusCode);
-            response.on("data", function(data){
-
-                const ipData= JSON.parse(data);
-                const city = ipData.city;
-                callback(city);
-                
-            })
-        })
-}
-    function handleResults(results){
-        const query= results;
-        const apiKey = process.env.WEATHER_API_KEY;
-        const unit= "metric"
-        //api url for the weather api
-        const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
-
-        function makeCall2(weatherUrl, callback){
-
-            https.get(weatherUrl, function(response){
-                response.on("data", function(data){
-                    //converts data to json
-                    const weatherData = JSON.parse(data);
-                    const icon = weatherData.weather[0].icon;
-                    const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
-                    callback(imageURL);
-                })
-            })
-        }
-
-        //THE RESULTS OF "makeCall2"
-        //SENDS THE WEATHER INFO USING "results2"
-        function handleResults2(results2){
-            let date=data.getDay();
-            let time= data.getTime();
-            if(req.body != null){
-                //creates a new post
-                const post=new Note({
-                    title: req.body.title,
-                    content: req.body.content,
-                    owner: req.user.username,
-                    state: "normal-mode",
-                    date: date,
-                    time: time,
-                    imageURL: results2,
-                    shared: "false",
-
-
-                });
-            
-                //saving the information entered into the note document 
-                post.save();
-            }
-
-            res.redirect("/pages");
-
-        }
-        makeCall2(weatherUrl, function(results2) {
-            handleResults2(results2);
-        })
-        }
-    makeCall(ipinfoApi, function(results) {
-        
-        handleResults(results);
-    })
-
-     
-
-
- }
-
-);
-
-
-//--------User page Buttons
-
-//--eraser icon request--deletes the entry from the noteuser collection
-//and marks it as deleted inside of the notes collection
-app.put("/pages/delete",isLoggedIn, async(req, res)=>{
-    //distinguishing things to delete
-    const clickedEntry = req.body.deleteEntry;
-
-        Note.updateOne(
-            {_id: clickedEntry},
-            {$set: {"deleted": "true"}},
-            function(err) {
-                if(err) {
-                    console.log(err);
-                }
-            }
-            )
-        //Finds the entry with the id of the currently logged in user
-        //looks at the notebookContents array and finds the id inside that 
-        //corresponds to the clickedEntry (the delete button that corresponds to the entry)
-        //then it excludes it from the list after the update
-        //this causes the items to be erased from the NoteUser collection.
-        NoteUser.findOneAndUpdate({_id:req.user.id},
-            {$pull:{noteBookContents:{_id: clickedEntry}}},
-            {new:true, useFindAndModify: false},
-            function(err){
-                if(err){
-                    console.log(err);
-                }else{
-                    res.redirect("/pages");
-                    
-                }
-            }
-            );
-
-})
-
-//--pen icon request-used to hide and unhide the edit form
-app.put("/pages/pre-edit",isLoggedIn, async(req, res)=>{
-
-    //gets information sent by the editEntry/pen button
-    const editState = req.body.editEntry;
-    console.log(editState);
-    //find the state of the entry being clicked and toggle it between edit-mode and normal-mode
-    Note.findById(editState, function(err, foundEntry){
-        if(err){
-            console.log(err);
-        }else{
-            if(foundEntry.state == "edit-mode"){
-                Note.updateOne(
-                    {_id: editState},
-                    {$set: {"state": "normal-mode"}},
-                    function(err){
-                        if(err){
-                            console.log(err);
-                        }else{
-                           
-                            res.redirect("/pages");
-                        }
-                    }        
-                )                
-            }else{
-                Note.updateOne(
-                    {_id: editState},
-                    {$set: {"state": "edit-mode"}},
-                    function(err){
-                        if(err){
-                            console.log(err);
-                        }else{
-                            
-                            res.redirect("/pages");
-                        }
-                    }        
-                )                
-            }
-        }
-    })
-
-})
-
- 
-//--edit button request- used to edit user entries
-app.put("/pages/edit",isLoggedIn, async(req,res)=> {
-    const toEdit = req.body.Edit;
-    const title= req.body.title2;
-    const content=req.body.content2;
-
-        //updates the title and content of the note
-        Note.updateOne(
-            {_id: toEdit},
-            {$set: {"title":title, "content":content }},
-            function(err){
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log("edit page button pressed");
-                   
-                }
-            }
-        );
-        //updates the title and content of the noteUser
-        NoteUser.updateOne(
-        {_id: req.user.id, "noteBookContents":{"$elemMatch": {"_id": toEdit}}},
-        {$set: {"noteBookContents.$.title":title, "noteBookContents.$.content":content }},
-        function(err){
-            if(err){
-                console.log(err);
-            }else{
-                console.log("edit page button pressed");
-                
-            }
-         }
-    );
-         //updates the state everytime the edit page button is clicked on
-        Note.updateOne(
-            {_id: toEdit},
-            {$set: {"state": "normal-mode"}},
-            function(err){
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log("edit page button pressed");
-                    res.redirect("/pages");
-                }
-            }        
-        )
-   
-
-})
-
-//--last icon (arrow in a box) request to share and unshare user entries
-app.put("/pages/share-unshare",isLoggedIn,level1Access, async(req, res)=> {
-    const toShare= req.body.share;
-    //finding the entry by its id
-    Note.findById(toShare, function(err, foundNoteEntry) {
-        if(err){
-            console.log(err);
-        }else{
-            //checking the shared state
-            //if the state is false when clicked change it to true
-            //otherwise change it to false
-            if(foundNoteEntry.shared === "false") {
-                Note.updateOne(
-                    {_id: toShare},
-                    {$set: {"shared": "true"}},
-                    function(err){
-                        if(err){
-                            console.log(err);
-                        }else{
-                            res.redirect("/pages")
-                        }
-                    }
-            ) 
-            }else{
-                Note.updateOne(
-                    {_id:toShare},
-                    {$set: {"shared": "false"}},
-                    function(err){
-                        if(err){
-                            console.log(err);
-                        }else{
-                            res.redirect("/pages")
-                        }
-                    }
-                )    
-            }
-        }
-    })
-
-})
-
-//Go to page buttonn request
-//---this creates multiple new pages for the users page
-//creating a page to show the entries of users
-app.get("/pages/:id",isLoggedIn, async(req,res)=>{
-    
-
-        //gets the title of the page that is going to be
-        //created after the click --create page
-        const pageEntry = req.params.id;
-
-        const theUser = req.user.username;
-
-        NoteUser.findById(req.user.id, function(err, findpic){
-            if(err){
-                console.log(err);
-            }else{
-                
-        //search for the record with the same username as the currently logged in user
-        NoteUser.findOne({"username": theUser}).populate('noteBookContents').exec(function(err, post){
-            if(err){
-                console.log(err);
-            }else{
-                const newPage = post.noteBookContents;
-                const pic= findpic.profileImage.url;
-                //renders the userContent page
-                //the data passed into it is the title of the page that the user is looking for
-                //also the contents of the relavant noteBookContents of the found post     
-                res.render("userContent",{pic,newPage:newPage, pageEntry: pageEntry});    
-
-            }
-        
-
-        })
-    }});
-
-
-})
-
-
-
-//--------------------------------------------------------
-
-
-
-
-
-//--------------------Public page------------------------------------
-app.get("/public-pages",isLoggedIn,level1Access, async(req,res)=>{
-
-        
-        // finding the document of the current user for the purpos of getting the url
-        NoteUser.findById(req.user.id, function(err, findpic){
-            if(err){
-                console.log(err);
-            }else{
-                
-
-                //finding the user related entries by the id of currently logged in user
-                Note.find({"shared": {$eq: "true"}, "deleted":{$ne: "true"}}, function(err, publicPosts){
-
-                    if(err){
-                        console.log(err);
-                        
-                                
-                    }else{
-
-                            const pic= findpic.profileImage.url;                
-                            res.render("publicPage", {pic, publicPosts: publicPosts});
-                        
-                    }
-                });
-            }});
- 
-});
-
-
-//----------------Creating multiple new pages for the public page
-//creating a page to show the entries of users
-app.get("/public-pages/:id", isLoggedIn, async(req,res)=>{
-    
-    
-    //gets the title of the page that is going to be
-    //created after the click --create page
-    const pageEntry = req.params.id;
-    const currentUser = req.user.username;
-
-    console.log(currentUser)
-
-
-            Note.findById(pageEntry, function(err, foundEntry){
-                if(err){
-                    console.log(err);
-                }else{
-                    if(foundEntry){
-                        // finding the document of the current user for the purpos of getting the url
-                        NoteUser.findById(req.user.id, function(err, findpic){
-                            if(err){
-                                console.log(err);
-                            }
-                            else{
-                                Review.find({"target":pageEntry}, function(err, foundReview){
-                                    if(!err){
-                                        
-                                        foundEntry.reviews= foundReview;
-                                        
-                                        foundEntry.save(function(){
-                                        //search for all records 
-                                        Note.find({}).populate('reviews').exec(function(err, post){
-                                        const newPublicContent = post;
-                                        const pic = findpic.profileImage.url;
-                                        //renders the publicContent page
-                                        //the data passed into it is the title of the page that the user is looking for
-                                        //also the contents of the relavant noteBookContents of the found post     
-                                        res.render("publicContent",{pic,newPublicContent:newPublicContent, pageEntry: pageEntry, currentUser: currentUser});
-
-                                        })    
-                                        
-                                        });                        
-                                    }
-
-                            })
-                             }});
-                        }
-                    }
-                })
-            
-
-})
-
-//--reviews section of public page
-app.put("/public-pages/:id/review",isLoggedIn,level2Access, async(req, res)=>{
-    const id= req.params.id;
-    console.log("EQWEQWE: "+ id);
-    const pageEntry = id;
-    const content=req.body.content;
-     console.log("afadfa");
-
-             
-    if(req.body != null){
-
-    const review = new Review({
-        content: content,
-        target:pageEntry,
-        author: req.user.username 
-    });
-    review.save();
-}
-
-        res.redirect("/public-pages/"+pageEntry);
-
-
-})
-
-app.delete("/public-pages/:id/review/delete",isLoggedIn,level2Access, async(req, res)=>{
-    const id= req.params.id;    
-    const clickedEntry = id;
-       
-        Review.findByIdAndRemove({_id:clickedEntry, author:{$eq:req.user.username}}, function(err, found){
-            if(err){
-                console.log(err);
-            }else{
-                if(found)
-                //redirects to the page where the target of the review is
-                 res.redirect("/public-pages/"+found.target);
-
-            }
-        });
-   
-
-})
 
 
 //-----SETTINGS PAGE--------------------------------------------------------------------
