e2ad1a45af685c2e682e7b165d914903da2b057c
Jereen <jader9e1@hotmail.com>
2023-01-28 17:17:48 -0600

    "fix:Refactor code and add error handler"

--- routes/pages.js
+++ routes/pages.js
@@ -6,44 +6,62 @@ const https = require("https");
 const {isLoggedIn} = require('../middleware/login_middlewaare');
 const {level1Access, level2Access}= require('../middleware/access_middleware');
 const data = require("../data");
+const CustomError = require('../middleware/CustomError');
+const catchAsync = require('../middleware/catchAsync');
+const { nextTick } = require('process');
 
 //-------------------MAIN PAGE FOR USER NOTES--------------------
-router.get("/", isLoggedIn, async(req,res)=>{
+router.get("/", isLoggedIn, catchAsync(async(req,res)=>{
 
-
+        //find current user by username, save the associated entry to noteuser
+        //find the note of the user whose deleted field is not marked as true
+        //find the url of the profile image inside of pic 
+        //assign the found note to the noteBook contents array
+        //save the noteuser entry
+        //render the page using the things found
         const theUser = req.user.username;
+        const noteuser = await NoteUser.findById(req.user.id);
+        const note = await Note.find({"owner":theUser, "deleted":{$ne:"true"}});
+        const pic= noteuser.profileImage.url;
+        noteuser.noteBookContents= note;
+        await noteuser.save();
+        res.render('page',{pic, messages: req.flash('success'), userContent: noteuser, theUser: theUser });
 
 
-        //finds the current user using the user.id provided by the passport package
-        NoteUser.findById(req.user.id,function(err, foundUser) {
-            if(err){
-                console.log(err);
-            }else{
+        // const theUser = req.user.username;
 
-                if(foundUser){
 
-                    //looking for the data in the notes collection where the owner name
-                    //is the same as the username of the currently logged in user
-                    Note.find({"owner": req.user.username, "deleted":{$ne:"true"}}, function(err, user2) {
+        // //finds the current user using the user.id provided by the passport package
+        // NoteUser.findById(req.user.id,function(err, foundUser) {
+        //     if(err){
+        //         console.log(err);
+                
+        //     }else{
 
-                        const pic= foundUser.profileImage.url;
+        //         if(foundUser){
+
+        //             //looking for the data in the notes collection where the owner name
+        //             //is the same as the username of the currently logged in user
+        //             Note.find({"owner": req.user.username, "deleted":{$ne:"true"}}, function(err, user2) {
+
+        //                 const pic= foundUser.profileImage.url;
                                             
-                        //saves the notes associated with the user into the notebooks array
-                        foundUser.noteBookContents=user2;
+        //                 //saves the notes associated with the user into the notebooks array
+        //                 foundUser.noteBookContents=user2;
 
-                        //saves the userinfo into noteUser collection and renders the page
-                        foundUser.save(function(){
+        //                 //saves the userinfo into noteUser collection and renders the page
+        //                 foundUser.save(function(){
 
-                            res.render("page", {pic, messages: req.flash('success'), userContent: foundUser, theUser: theUser});
+        //                     res.render("page", {pic, messages: req.flash('success'), userContent: foundUser, theUser: theUser});
                                 
-                        })
-                    })
-                }
-            }
-        });
+        //                 })
+        //             })
+        //         }
+        //     }
+        // });
   
 
-});
+}));
 
 //renders the notebook user page
 router.post("/",isLoggedIn, async(req,res)=>{
@@ -52,8 +70,10 @@ router.post("/",isLoggedIn, async(req,res)=>{
 //CALLBACK FUNCTIONS  
     //USING AN IP ADDRESS FINDER TO GET THE CITY NAME OF THE USER
     token=process.env.IPINFO_TOKEN;
+    //api endpoint with the access token
     const ipinfoApi ="https://ipinfo.io?token="+token;
 
+    //using the function call the api 
     function makeCall (ipinfoApi, callback){
 
         https.get(ipinfoApi, function(response){
@@ -115,10 +135,12 @@ router.post("/",isLoggedIn, async(req,res)=>{
             res.redirect("/pages");
 
         }
+
         makeCall2(weatherUrl, function(results2) {
             handleResults2(results2);
         })
         }
+
     makeCall(ipinfoApi, function(results) {
         
         handleResults(results);
@@ -136,38 +158,44 @@ router.post("/",isLoggedIn, async(req,res)=>{
 
 //--eraser icon request--deletes the entry from the noteuser collection
 //and marks it as deleted inside of the notes collection
-router.put("/delete",isLoggedIn, async(req, res)=>{
-    //distinguishing things to delete
+router.put("/delete",isLoggedIn, catchAsync(async(req, res)=>{
     const clickedEntry = req.body.deleteEntry;
+    await Note.updateOne({_id:clickedEntry}, {"deleted": "true"});
+    // await NoteUser.findOneAndUpdate(req.user.id, {noteBookContents:{_id: clickedEntry}})
+    res.redirect("/pages");
 
-        Note.updateOne(
-            {_id: clickedEntry},
-            {$set: {"deleted": "true"}},
-            function(err) {
-                if(err) {
-                    console.log(err);
-                }
-            }
-            )
-        //Finds the entry with the id of the currently logged in user
-        //looks at the notebookContents array and finds the id inside that 
-        //corresponds to the clickedEntry (the delete button that corresponds to the entry)
-        //then it excludes it from the list after the update
-        //this causes the items to be erased from the NoteUser collection.
-        NoteUser.findOneAndUpdate({_id:req.user.id},
-            {$pull:{noteBookContents:{_id: clickedEntry}}},
-            {new:true, useFindAndModify: false},
-            function(err){
-                if(err){
-                    console.log(err);
-                }else{
-                    res.redirect("/pages");
+
+    // //distinguishing things to delete
+    // const clickedEntry = req.body.deleteEntry;
+
+    //     Note.updateOne(
+    //         {_id: clickedEntry},
+    //         {$set: {"deleted": "true"}},
+    //         function(err) {
+    //             if(err) {
+    //                 console.log(err);
+    //             }
+    //         }
+    //         )
+    //     //Finds the entry with the id of the currently logged in user
+    //     //looks at the notebookContents array and finds the id inside that 
+    //     //corresponds to the clickedEntry (the delete button that corresponds to the entry)
+    //     //then it excludes it from the list after the update
+    //     //this causes the items to be erased from the NoteUser collection.
+    //     NoteUser.findOneAndUpdate({_id:req.user.id},
+    //         {$pull:{noteBookContents:{_id: clickedEntry}}},
+    //         {new:true, useFindAndModify: false},
+    //         function(err){
+    //             if(err){
+    //                 console.log(err);
+    //             }else{
+    //                 res.redirect("/pages");
                     
-                }
-            }
-            );
+    //             }
+    //         }
+    //         );
 
-})
+}));
 
 //--pen icon request-used to hide and unhide the edit form
 router.put("/pre-edit",isLoggedIn, async(req, res)=>{
