9b3cc0b046709d171eb1d804a3d875aef1ed1f49
Jereen <jader9e1@hotmail.com>
2023-02-08 13:33:13 -0600

    "feat: Add benefits array to store the list of subscription benefits"

--- models/Subscription.js
+++ models/Subscription.js
@@ -5,7 +5,8 @@ const subscriptionSchema = new mongoose.Schema ({
     price: Number,
     type: String,
     imageUrl: String,
-    priceId: String
+    priceId: String,
+    benefits:[String]
 })
 
 const Subscription = new mongoose.model("Subscription", subscriptionSchema);
